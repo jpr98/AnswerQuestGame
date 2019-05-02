9c6a11fb2f6462310c830b93ad3d6b52b3576356
JuanPablo Ramos <jprscs@gmail.com>
2019-05-01 21:40:20 -0500

    Refactors Assets and adds pause screens

--- src/answerquestgame/Helpers/Assets.java
+++ src/answerquestgame/Helpers/Assets.java
@@ -8,40 +8,42 @@ import java.awt.image.BufferedImage;
 import java.nio.Buffer;
 
 public class Assets {
-    //  IMAGES
-    public static BufferedImage backgroundOne;
-    public static BufferedImage backgroundTwo;
-    public static BufferedImage button;
-    public static BufferedImage buttonClicked;
-    public static BufferedImage timer;
     
+    // LEVEL 1
+    public static BufferedImage backgroundOne;
+    public static BufferedImage balloonSprites;
+    public static BufferedImage balloonMoving[];
+    public static BufferedImage balloonFallingSprites;
+    public static BufferedImage balloonFalling[];
+    public static BufferedImage level1Pause;
     public static BufferedImage level1Player1Win;
     public static BufferedImage level1Player2Win;
+    
+    // LEVEL 2
+    public static BufferedImage backgroundTwo;
+    public static BufferedImage rocketSprites;
+    public static BufferedImage rocketMoving[];
+    public static BufferedImage rocketFallingSprites;
+    public static BufferedImage rocketFalling[];
+    public static BufferedImage level2Pause;
     public static BufferedImage level2Player1Win;
     public static BufferedImage level2Player2Win;
     
-    public static BufferedImage nextLevelButton;
-    public static BufferedImage nextLevelButtonClicked;
-    
-    
-    // MENU IMAGES
+    // MENU
     public static BufferedImage startButton;
     public static BufferedImage startButtonClicked;
     public static BufferedImage tutorialButton;
     public static BufferedImage tutorialButtonClicked;
     public static BufferedImage menuBackground;
-    
-    //  ANIMATION
     public static BufferedImage titleSprites;
     public static BufferedImage titleMoving[];
-    public static BufferedImage balloonSprites;
-    public static BufferedImage balloonMoving[];
-    public static BufferedImage balloonFallingSprites;
-    public static BufferedImage balloonFalling[];
-    public static BufferedImage rocketSprites;
-    public static BufferedImage rocketMoving[];
-    public static BufferedImage rocketFallingSprites;
-    public static BufferedImage rocketFalling[];
+    
+    // GENERAL
+    public static BufferedImage button;
+    public static BufferedImage buttonClicked;
+    public static BufferedImage timer;
+    public static BufferedImage nextLevelButton;
+    public static BufferedImage nextLevelButtonClicked;
 
     //  SOUNDS
 
@@ -50,20 +52,45 @@ public class Assets {
      * Assets initializer, to be called in Game
      */
     public static void init() {
-        //  IMAGES
-        backgroundOne = ImageLoader.loadImage("/images/level1.png");
-        backgroundTwo = ImageLoader.loadImage("/images/level2.png");
-        button = ImageLoader.loadImage("/images/buttonNormal.png");
-        buttonClicked = ImageLoader.loadImage("/images/buttonClicked.png");
-        timer = ImageLoader.loadImage("/images/timer.png");
+        // LEVEL 1
+        backgroundOne = ImageLoader.loadImage("/images/Level1/level1.png");
+        level1Player1Win = ImageLoader.loadImage("/images/Level1/level1player1.png");
+        level1Player2Win = ImageLoader.loadImage("/images/Level1/level1player2.png");
+        level1Pause = ImageLoader.loadImage("/images/Level1/level1Pause.png");
         
-        level1Player1Win = ImageLoader.loadImage("/images/level1player1.png");
-        level1Player2Win = ImageLoader.loadImage("/images/level1player2.png");
-        level2Player1Win = ImageLoader.loadImage("/images/level2player1.png");
-        level2Player2Win = ImageLoader.loadImage("/images/level2player2.png");
+        balloonSprites = ImageLoader.loadImage("/images/Level1/balloon.png");
+        SpriteSheet balloonSpriteSheet = new SpriteSheet(balloonSprites);
+        balloonMoving = new BufferedImage[4];
+        for (int i=0; i<4; i++) {
+            balloonMoving[i] = balloonSpriteSheet.crop(0, i * 64, 64, 64);
+        }
         
-        nextLevelButton = ImageLoader.loadImage("/images/nextLevelButtonNormal.png");
-        nextLevelButtonClicked = ImageLoader.loadImage("/images/nextLevelButtonClicked.png");
+        balloonFallingSprites = ImageLoader.loadImage("/images/Level1/poppedBalloon.png");
+        SpriteSheet balloonFallingSpriteSheet = new SpriteSheet(balloonFallingSprites);
+        balloonFalling = new BufferedImage[5];
+        for (int i=0; i<5; i++) {
+            balloonFalling[i] = balloonFallingSpriteSheet.crop(0, i * 64, 64, 64);
+        }
+        
+        // LEVEL 2
+        backgroundTwo = ImageLoader.loadImage("/images/Level2/level2.png");
+        level2Player1Win = ImageLoader.loadImage("/images/Level2/level2player1.png");
+        level2Player2Win = ImageLoader.loadImage("/images/Level2/level2player2.png");
+        level2Pause = ImageLoader.loadImage("/images/Level2/level2Pause.png");
+        
+        rocketSprites = ImageLoader.loadImage("/images/Level2/rocket.png");
+        SpriteSheet rocketSpriteSheet = new SpriteSheet(rocketSprites);
+        rocketMoving = new BufferedImage[3];
+        for (int i=0; i<3; i++) {
+            rocketMoving[i] = rocketSpriteSheet.crop(0, i * 80, 64, 80);
+        }
+        
+        rocketFallingSprites = ImageLoader.loadImage("/images/Level2/rocketFalling.png");
+        SpriteSheet rocketFallingSpriteSheet = new SpriteSheet(rocketFallingSprites);
+        rocketFalling = new BufferedImage[6];
+        for (int i=0; i<6; i++) {
+            rocketFalling[i] = rocketFallingSpriteSheet.crop(0, i * 160, 128, 160);
+        }
         
         // MENU
         startButton = ImageLoader.loadImage("/images/Menu/startButtonNormal.png");
@@ -72,7 +99,6 @@ public class Assets {
         tutorialButtonClicked = ImageLoader.loadImage("/images/Menu/tutorialButtonClicked.png");
         menuBackground = ImageLoader.loadImage("/images/Menu/menu.png");
         
-        //  ANIMATION   
         titleSprites = ImageLoader.loadImage("/images/Menu/answerQuest.png");
         SpriteSheet titleSpriteSheet = new SpriteSheet(titleSprites);
         titleMoving = new BufferedImage[4];
@@ -80,32 +106,12 @@ public class Assets {
             titleMoving[i] = titleSpriteSheet.crop(0, i * 60, 400, 60);
         }
         
-        balloonSprites = ImageLoader.loadImage("/images/balloon.png");
-        SpriteSheet balloonSpriteSheet = new SpriteSheet(balloonSprites);
-        balloonMoving = new BufferedImage[4];
-        for (int i=0; i<4; i++) {
-            balloonMoving[i] = balloonSpriteSheet.crop(0, i * 64, 64, 64);
-        }
-        
-        balloonFallingSprites = ImageLoader.loadImage("/images/poppedBalloon.png");
-        SpriteSheet balloonFallingSpriteSheet = new SpriteSheet(balloonFallingSprites);
-        balloonFalling = new BufferedImage[5];
-        for (int i=0; i<5; i++) {
-            balloonFalling[i] = balloonFallingSpriteSheet.crop(0, i * 64, 64, 64);
-        }
-        
-        rocketSprites = ImageLoader.loadImage("/images/rocket.png");
-        SpriteSheet rocketSpriteSheet = new SpriteSheet(rocketSprites);
-        rocketMoving = new BufferedImage[3];
-        for (int i=0; i<3; i++) {
-            rocketMoving[i] = rocketSpriteSheet.crop(0, i * 80, 64, 80);
-        }
-        
-        rocketFallingSprites = ImageLoader.loadImage("/images/rocketFalling.png");
-        SpriteSheet rocketFallingSpriteSheet = new SpriteSheet(rocketFallingSprites);
-        rocketFalling = new BufferedImage[6];
-        for (int i=0; i<6; i++) {
-            rocketFalling[i] = rocketFallingSpriteSheet.crop(0, i * 160, 128, 160);
-        }
+        // GENERAL
+        button = ImageLoader.loadImage("/images/buttonNormal.png");
+        buttonClicked = ImageLoader.loadImage("/images/buttonClicked.png");
+        timer = ImageLoader.loadImage("/images/timer.png");
+        nextLevelButton = ImageLoader.loadImage("/images/nextLevelButtonNormal.png");
+        nextLevelButtonClicked = ImageLoader.loadImage("/images/nextLevelButtonClicked.png");
+
     }
 }
